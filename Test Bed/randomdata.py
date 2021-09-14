6b9ca0ed562d0f767c003766c4c7f464b57e91db
VertigoK <ke.kevin.kim@gmail.com>
2021-09-14 23:20:54 +0900

    add randomdata.py

---
+++ Test Bed/randomdata.py
@@ -0,0 +1,17 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# x = np.arange(1000)
+s1 = np.random.normal(40, 0.01, 1000)
+s2 = np.random.normal(40, 0.01, 1000)
+
+
+
+# fig, axes = plt.subplots(2,1)
+
+# sns.distplot(s1, ax=axes[0])
+# sns.distplot(s2, ax=axes[1])
+# plt.show()
+# %%
